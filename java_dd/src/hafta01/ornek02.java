e94c6d01b2bb11116b3ece238df0395d1954233d
Caner <Caner@10.50.13.148>
2014-12-05 15:14:01 +0200

    xsxas

---
+++ java_dd/src/hafta01/ornek02.java
@@ -0,0 +1,22 @@
+package hafta01;
+
+class DortIslem{
+	
+	public DortIslem()
+	{
+		System.out.println("Constructor Method");
+		
+	}
+	
+	
+	
+}
+
+public class ornek02 {
+
+	public static void main(String[] args) {
+		DortIslem ilkIslem=new DortIslem();
+
+	}
+
+}
